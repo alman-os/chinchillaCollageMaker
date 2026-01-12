45f68f8a1737aecd9beee072b1244a70f144ff3d
Alman D. Gonzalez Pogromsky G <research@alman-os.com>
2026-01-12 02:33:18 -0500

    Initial Commit

---
+++ chinchillaCollage/chinchillaCollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  chinchillaCollageApp.swift
+//  chinchillaCollage
+//
+//  Created by Tom Riddle on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct chinchillaCollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
